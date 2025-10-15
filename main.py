6cb76afbb41db31d3df1e15cfdf72587f5425357
henkka008 <henkka008@hotmail.com>
2025-10-15 15:46:03 +0300

    Created main.py and books directory.

---
+++ main.py
@@ -0,0 +1,7 @@
+def get_books_text(filePath):
+    with open(filePath) as f:
+        file_contents = f.read()
+        return print(file_contents)
+    
+
+get_books_text('/home/kali/bootdev/bookbot/books/frankenstein.txt')
