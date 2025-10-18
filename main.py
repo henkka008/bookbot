09c32cd42a7a7f141df73aad1232350aecf65dbd
henkka008 <henkka008@hotmail.com>
2025-10-18 17:54:01 +0300

    Addedn print results

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ def main():
     text = get_book_text(file_path)
     num_words = get_num_words(text)
     counted_letters = kind_of_letters(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     sorted_letters = sorting(counted_letters)
     print(sorted_letters)
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
